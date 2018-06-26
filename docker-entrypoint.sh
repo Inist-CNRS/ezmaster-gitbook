1192d5325c8daa4499a79cc2e032d5b05b8aa2d5
thomas88100 <thomas.ehrhard2@etu.univ-lorraine.fr>
2018-06-26 14:50:36 +0200

    modification structuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,18 @@
 
 # inject config.json parameters to env
 # only if not already defined in env
-export GITHUB_URL=${GITHUB_URL:=$(jq -r -M .GITHUB_URL /config.json | grep -v null)}
+export GITHUB_URL_MARKDOWN=${GITHUB_URL_MARKDOWN:=$(jq -r -M .GITHUB_URL_MARKDOWN /config.json | grep -v null)}
+export GITHUB_URL_CONFIG=${GITHUB_URL_CONFIG:=$(jq -r -M .GITHUB_URL_CONFIG /config.json | grep -v null)}
 export BUILD_EACH_NBMINUTES=${BUILD_EACH_NBMINUTES:=$(jq -r -M .BUILD_EACH_NBMINUTES /config.json | grep -v null)}
 export SERVER_NAME=${SERVER_NAME:=$(jq -r -M .SERVER_NAME /config.json | grep -v null)}
 
 # Permit to change the server_name in the nginx configuration
 if [[ -z "$SERVER_NAME" ]]; then
-    sed -i "s/\(server_name\) \($SERVER_NAME\)\(;\)$/\1 localhost\3/g" nginx.conf
-    sed -i "s/\(server_name_in_redirect\) \([a-zA-Z]\+\\)\(;\)$/\1 off\3/g" nginx.conf
+    sed -i "s/\(server_name\) \($SERVER_NAME\)\(;\)$/\1 localhost\3/g" /etc/nginx/nginx.conf
+    sed -i "s/\(server_name_in_redirect\) \([a-zA-Z]\+\\)\(;\)$/\1 off\3/g" /etc/nginx/nginx.conf
 else
-    sed -i "s/\(server_name\) \(.*\)\(;\)$/\1 ${SERVER_NAME}\3/g" nginx.conf
-    sed -i "s/\(server_name_in_redirect\) \([a-zA-Z]\+\\)\(;\)$/\1 on\3/g" nginx.conf
+    sed -i "s/\(server_name\) \(.*\)\(;\)$/\1 ${SERVER_NAME}\3/g" /etc/nginx/nginx.conf
+    sed -i "s/\(server_name_in_redirect\) \([a-zA-Z]\+\\)\(;\)$/\1 on\3/g" /etc/nginx/nginx.conf
 fi
 
 # pull periodically to check if there are some doc modifications
@@ -20,6 +21,11 @@ fi
 pull.periodically.sh &
 
 # a watcher that build if they are any changes
+while true; do
+    if [[ -f /tmp/CloneDebugEffectue ]]; then
+        break
+    fi
+done
 watcher.sh &
 
 exec nginx -g 'daemon off;'
--- pull.periodically.sh
+++ pull.periodically.sh
@@ -1,38 +1,79 @@
 #!/bin/bash
 
+git clone https://github.com/git-hook/post-clone /tmp/post-clone
+
 if [[ $GITBOOK_DEBUG -eq 1 ]]
 then
-    if [[ ! -d "/app/src/.git" ]];then
-        git clone --verbose --progress ${GITHUB_URL} /app/src
+    if [[ ! -d "/app/doc/.git" ]];then
+        git clone --verbose --progress ${GITHUB_URL_MARKDOWN} /app/doc
+    else
+        cd /app/doc
+        git pull
+    fi
+    if [[ ! -d "/app/config/.git" ]];then
+        git clone --verbose --progress --template=/tmp/post-clone ${GITHUB_URL_CONFIG} /app/config
+        touch /tmp/CloneDebugEffectue
+    else
+        cd /app/config
+        git pull
     fi
 
     echo "######################################################"
     echo "#  DEBUG MODE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   #"
     echo "######################################################"
     chown -R $USERID:$GROUPID /app
+
 else
 
-    if [[ ! -d "/app/src/.git" ]];then
-        git clone --verbose --progress ${GITHUB_URL} /app/src
+    # if [[ ! -d "/app/src/.git" ]];then
+    #     git clone --verbose --progress ${GITHUB_URL} /app/src
+    # else
+    #     rm -rf /app/src/
+    #     git clone --verbose --progress ${GITHUB_URL} /app/src
+    # fi
+    if [[ ! -d "/app/doc/.git" ]];then
+        git clone --verbose --progress ${GITHUB_URL_MARKDOWN} /app/doc
     else
-        rm -rf /app/src/
-        git clone --verbose --progress ${GITHUB_URL} /app/src
+        rm -rf /app/doc
+        git clone --verbose --progress ${GITHUB_URL_MARKDOWN} /app/doc
     fi
+    if [[ ! -d "/app/config/.git" ]];then
+        git clone --verbose --progress ${GITHUB_URL_CONFIG} /app/config
+        touch /tmp/CloneDebugEffectue
+    else
+        rm -rf /app/config
+        git clone --verbose --progress ${GITHUB_URL_CONFIG} /app/config
+        touch /tmp/CloneDebugEffectue
+    fi
+
     echo "######################################################"
     echo "#  PROD MODE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    #"
     echo "######################################################"
 
-    cd /app/src/
     while true
     do
+        # Partie doc
+        cd /app/doc/
         git reset --hard HEAD
-        stdout=$(git pull 2>&1)
-        if [ "$stdout" == "Already up-to-date." ]
+        stdoutDoc=$(git pull 2>&1)
+        if [ "$stdoutDoc" == "Already up-to-date." ]
         then
             echo "Doc already up-to-date"
         else
             echo "Ongoing update"
         fi
+
+        # Partie config
+        cd /app/config/
+        git reset --hard HEAD
+        stdoutConfig=$(git pull 2>&1)
+        if [ "$stdoutConfig" == "Already up-to-date." ]
+        then
+            echo "Config already up-to-date"
+        else
+            echo "Ongoing update"
+        fi
+
         echo "Waiting $BUILD_EACH_NBMINUTES minutes before next verification."
         sleep ${BUILD_EACH_NBMINUTES}m
     done
